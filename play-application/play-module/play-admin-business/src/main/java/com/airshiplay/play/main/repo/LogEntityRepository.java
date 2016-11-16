29a18dea944418eb3a1a8fec28bb52213860deb5
lig <lig@certusnet.com.cn>
2016-11-16 18:08:50 +0800

    log error

--- play-application/play-module/play-admin-business/src/main/java/com/airshiplay/play/main/repo/LogEntityRepository.java
+++ play-application/play-module/play-admin-business/src/main/java/com/airshiplay/play/main/repo/LogEntityRepository.java
@@ -1,6 +1,7 @@
 package com.airshiplay.play.main.repo;
 
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 import com.airshiplay.play.main.entity.LogEntity;
 import com.airshiplay.play.main.entity.QLogEntity;
@@ -8,8 +9,8 @@ import com.airshiplay.play.repo.jpa.BaseJpaRepository;
 
 public interface LogEntityRepository extends BaseJpaRepository<LogEntity, Long> ,QuerydslBinderCustomizer<QLogEntity>{
 
-//	@Override
-//	default public void customize(QuerydslBindings bindings, QLogEntity root){
-//		bindings.bind(root.createdDate).first((path,value) ->path.eq(value));
-//	}
+	@Override
+	default public void customize(QuerydslBindings bindings, QLogEntity root){
+		//bindings.bind(root.createdDate).first((path,value) ->path.eq(value));
+	}
 }
