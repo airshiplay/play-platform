4e40915dbc18e0166d336dc3e22dc0f519734bfc
lig <lig@certusnet.com.cn>
2017-08-16 13:34:11 +0800

    delete target

---
+++ play-framework/play-util/src/main/java/com/airshiplay/play/util/SystemEnvVariables.java
@@ -0,0 +1,44 @@
+package com.airshiplay.play.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+/**
+ * Created by lig on 17/6/9.
+ */
+public class SystemEnvVariables {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SystemEnvVariables.class);
+
+
+    public String getAppRoot() {
+        String appRoot = null;
+        appRoot = System.getProperty("catalina.base");
+        if(appRoot != null) {
+            appRoot = getCanonicalPath(appRoot);
+        }
+        return appRoot;
+    }
+
+    /**
+     * Gets the canonical path<br/>
+     *
+     * @param inPath input path
+     * @return the canonical path.
+     * @since
+     */
+    private String getCanonicalPath(final String inPath) {
+        String path = null;
+        try {
+            if(inPath != null) {
+                final File file = new File(inPath);
+                path = file.getCanonicalPath();
+            }
+        } catch(final IOException e) {
+            LOG.error("file.getCanonicalPath() IOException:", e);
+        }
+        return path;
+    }
+}
