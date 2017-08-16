2328adf115d1d763189e153c15e7b836f4b0dced
lig <lig@certusnet.com.cn>
2017-08-16 13:45:54 +0800

    delte

--- play-application/play-module/play-shop-business/src/main/java/com/airshiplay/play/shop/repo/BrandEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.airshiplay.play.shop.repo;
-
-import com.airshiplay.play.repo.jpa.BaseJpaRepository;
-import com.airshiplay.play.shop.entity.BrandEntity;
-
-public interface BrandEntityRepository extends BaseJpaRepository<BrandEntity, Long> {
-
-}
