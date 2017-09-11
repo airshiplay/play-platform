62ea742b85a0d6b46d363269cc486e77ca6f6e71
lig <lig@certusnet.com.cn>
2017-09-11 11:22:30 +0800

    update to 2.0.0-SNAPSHOT

---
+++ play-framework/play-xml/src/main/java/com/airlenet/xml/XmlConfigBean.java
@@ -0,0 +1,47 @@
+package com.airlenet.xml;
+
+import com.airlenet.play.repo.domain.Node;
+import com.airlenet.play.repo.domain.Result;
+import com.airlenet.play.repo.domain.Tree;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lig
+ * @date 17/9/8
+ */
+@Configuration
+public class XmlConfigBean {
+
+
+    @Bean
+    public XmlMapper objectMapper() {
+        XmlMapper xmlMapper  = new XmlMapper();
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        xmlMapper.registerModule(simpleModule());
+//        // objectMapper.setAnnotationIntrospector(ai)
+//
+//        objectMapper.addMixIn(Persistable.class, AntPathFilterMixin.class);
+//        objectMapper.addMixIn(List.class, AntPathFilterMixin.class);
+//        objectMapper.setSerializationInclusion(Include.NON_NULL);
+//        objectMapper.disable(SerializationFeature.FAIL_ON_SELF_REFERENCES);
+//
+//        SimpleFilterProvider filterPrvider=	new SimpleFilterProvider().addFilter("antPathFilter", new AntPathPropertyFilter(new String[] { "*","*.*" }));
+//        objectMapper.setFilterProvider(filterPrvider);
+        return xmlMapper;
+    }
+
+    @Bean
+    public JacksonXmlModule simpleModule() {
+        JacksonXmlModule simpleModule = new JacksonXmlModule();
+//        simpleModule.addSerializer(DateTime.class, new JodaDateTimeSerializer());
+//        simpleModule.addSerializer(Page.class, new PageSerializer<>());
+//        simpleModule.addSerializer(Tree.class, new TreeSerializer<>());
+//        simpleModule.addSerializer(Node.class, new NodeSerializer<>());
+        simpleModule.addSerializer(Result.class, new ResultSerializer());
+        return simpleModule;
+    }
+}
