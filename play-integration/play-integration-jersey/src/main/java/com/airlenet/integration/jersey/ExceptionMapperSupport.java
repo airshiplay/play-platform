09e5a218ccb789c7a1b357f4366b391e0efc61f7
airshiplay <lig@certusnet.com.cn>
2017-10-08 13:25:17 +0800

    update add jersey support

--- play-integration/play-integration-jersey/src/main/java/com/airlenet/integration/jersey/ExceptionMapperSupport.java
+++ play-integration/play-integration-jersey/src/main/java/com/airlenet/integration/jersey/ExceptionMapperSupport.java
@@ -1,5 +1,6 @@
 package com.airlenet.integration.jersey;
 
+import com.airlenet.repo.domain.PlayResult;
 import com.airlenet.repo.domain.Result;
 import com.airlenet.security.shiro.PlayAuthenticationException;
 import org.slf4j.Logger;
@@ -23,12 +24,12 @@ public class ExceptionMapperSupport implements ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception e) {
         if (e instanceof PlayAuthenticationException) {
-            return Response.status(Response.Status.UNAUTHORIZED).entity(Result.accessDenide()).build();
+            return Response.status(Response.Status.UNAUTHORIZED).entity(PlayResult.error("401",null)).build();
         }
         if(e instanceof NotFoundException){
-            return Response.status(Response.Status.NOT_FOUND).entity(Result.notFound()).build();
+            return Response.status(Response.Status.NOT_FOUND).entity(PlayResult.error("404",null)).build();
         }
         LOG.error("ExceptionMapperSupport", e);
-        return Response.status(Response.Status.BAD_REQUEST).entity(Result.exception()).build();
+        return Response.status(Response.Status.BAD_REQUEST).entity(PlayResult.error(e.getMessage(),null)).build();
     }
 }
